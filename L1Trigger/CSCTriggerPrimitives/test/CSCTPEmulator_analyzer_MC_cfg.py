920770fb571ad39a0180c98b855354b70fbd773c
Nik-Menendez <nikmenendez@ufl.edu>
2020-04-30 17:35:14 +0200

    Added rate counting scripts to this repository for testing

--- L1Trigger/CSCTriggerPrimitives/test/CSCTPEmulator_analyzer_MC_cfg.py
+++ L1Trigger/CSCTriggerPrimitives/test/CSCTPEmulator_analyzer_MC_cfg.py
@@ -9,7 +9,7 @@ process = cms.Process("ANA", eras.Run3)
 
 process.maxEvents = cms.untracked.PSet(
     #input = cms.untracked.int32(100)
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(2)
 )
 
 process.source = cms.Source("PoolSource",
